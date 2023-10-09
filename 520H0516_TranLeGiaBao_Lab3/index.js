90a26d89d21c577a303b24e1a7c8acd648d294ae
huy.ntq <hyquaq@gmail.com>
2023-10-09 11:48:17 +0700

    Update index.js

--- 520H0516_TranLeGiaBao_Lab3/index.js
+++ 520H0516_TranLeGiaBao_Lab3/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const multer = require('multer');
 const emailValidator = require('email-validator');
-const fn = require('./function')
+const fn = require('./function');
 require('dotenv').config();
 const PORT = process.env.PORT;
 const EMAIL = process.env.EMAIL;
@@ -13,23 +13,23 @@ const PASSWORD = process.env.PASSWORD;
 // SET STORAGE
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads')
+        cb(null, 'uploads');
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.fieldname + '-' + Date.now());
     }
-})
+});
 
 const upload = multer({
     storage: storage,
     dest: 'uploads/',
     fileFilter: function (req, file, callback) {
         if (file.mimetype.startsWith('image/')) {
-            callback(null, true)
+            callback(null, true);
         }
-        else callback(null, false)
+        else callback(null, false);
     }, limits: { fileSize: 50000 }
-})
+});
 
 
 
@@ -43,7 +43,7 @@ let products = [
     { id: 1, name: "iPhone 12 Pro", price: 30000000 },
     { id: 2, name: "iPhone 11", price: 17000000 },
     { id: 3, name: "iPhone Xr", price: 11000000 },
-]
+];
 
 // Session middleware
 app.use(session({
@@ -59,57 +59,51 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.render('index', { products })
-})
+    res.render('index', { products });
+});
 
 app.get('/login', (req, res) => {
     // res.send('Trang chu login')
-    res.render('login', { email: '', password: '', })
-})
+    res.render('login', { email: '', password: '', });
+});
 app.post('/login', (req, res) => {
     let body = req.body;
-    let err = ''
+    let err = '';
     if (!body.email) {
-        err = 'Please enter your email'
+        err = 'Please enter your email';
     }
     else if (!emailValidator.validate(body.email)) {
-        err = 'Email is not valid'
+        err = 'Email is not valid';
     }
     else if (!body.password) {
-        err = 'Please enter your password'
+        err = 'Please enter your password';
     }
     else if (body.password.length < 6) {
-        err = 'Password should have at least 6 character'
+        err = 'Password should have at least 6 character';
     }
     else if (body.email !== EMAIL || body.password !== PASSWORD) {
 
-        err = 'Invalid Email or Password'
+        err = 'Invalid Email or Password';
     }
     if (err.length > 0) {
-        res.render('login', { errorMessage: err })
+        res.render('login', { errorMessage: err });
     }
     else {
-        res.end('Login successful')
+        res.end('Login successful');
     }
-    return res.redirect('/')
-})
+    return res.redirect('/');
+});
 
 app.get('/add', (req, res) => {
-    res.render('add')
-})
+    res.render('add');
+});
 app.post('/add', (req, res) => {
-    const uploader = upload.single('image')
+    const uploader = upload.single('image');
     uploader(req, res, err => {
-        let image = req.file
+        let image = req.file;
         let { name, price, desc } = req.body;
         let errorMsg = fn.checkProductValidation(name, price, image, err, desc);
-        // if (!image) {
-        //     res.end('No image found')
-        // }
-        // else if (err) {
-        //     res.end('Image is too large')
-        // }
-        // else res.end('Xu ly thanh cong')
+
         if (errorMsg.length > 0) {
             return res.status(403).render('add', {
                 name,
@@ -126,15 +120,15 @@ app.post('/add', (req, res) => {
             desc,
             imgName: req.file.filename,
         });
-        fn.addFlashMessage(req,'success', 'Product added successfully');
-    })
-    return res.redirect('/');
-})
+        fn.addFlashMessage(req, 'success', 'Product added successfully');
+        return res.redirect('/');
+    });
+});
 
 
 
 app.use((req, res) => {
-    res.end('Lien ket nay khong duoc ho tro')
-})
+    res.end('Lien ket nay khong duoc ho tro');
+});
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
